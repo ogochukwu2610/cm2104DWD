2691fa96670103424c944e6742e087568ee9df0a
OGOCHUKWU EMELE (1405932) <1405932@rgu.ac.uk>
2018-03-12 16:48:45 +0000

    The third js

---
+++ lab13/mythirdserver.js
@@ -0,0 +1,7 @@
+var http=require('http');
+var oneLinerJoke=require('one-liner-joke');
+http.createServer(function (req,res){
+  res.writeHead(200,{'Content-Type': 'text/html'});
+  var randomJoke=oneLinerJoke.getRandomJoke();
+  res.end('randomJoke.body');
+}) .listen(8080);
