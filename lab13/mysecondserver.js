cc910a3bc29b4d2a82c313dfc67ef1ae557ae55b
OGOCHUKWU EMELE (1405932) <1405932@rgu.ac.uk>
2018-03-12 16:34:15 +0000

    Test commit

--- lab13/mysecondserver.js
+++ lab13/mysecondserver.js
@@ -2,6 +2,6 @@ var http=require('http');
 var currentdate = require('./mymodule');
 http.createServer(function (req,res){
   res.writeHead(200,{'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + currentdate.mydateTime());
+  res.write("The date and time are currently: " + currentdate.myDateTime());
   res.end('Hello World!');
 }) .listen(8080);
